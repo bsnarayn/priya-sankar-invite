ed7c46dbf91c2525b973a567f8f79fd8a4145086
gitadmin <bsankaranarayan@gmail.com>
2026-06-28 01:06:36 +0530

    Final security checks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   output: "static",
   adapter: cloudflare(),
   integrations: [mdx()],
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
 });
